36fc56eca1cb92e26d871d3ff558c493d54015b1
mhdshabeebvp <sshabeeb33@gmail.com>
2024-03-12 20:22:12 +0530

    front-end part is completed

--- stockproject/mainapp/views.py
+++ stockproject/mainapp/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def stockPicker(request):
     return render(request, "mainapp/stockpicker.html")
+def stockTracker(request):
+    return render(request, "mainapp/stocktracker.html")
